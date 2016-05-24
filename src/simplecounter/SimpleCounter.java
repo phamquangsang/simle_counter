3a61f0987f93c1e6f42ac61c8a496bd0c7bf800a
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-24 09:15:52 +0700

    them thong ke thoi gian

--- src/simplecounter/SimpleCounter.java
+++ src/simplecounter/SimpleCounter.java
@@ -15,9 +15,12 @@ public class SimpleCounter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        long time = System.currentTimeMillis();
         for(long i=1 ; i<=200;++i){
             System.out.print(""+i+" ");
         }
+        long time1 = System.currentTimeMillis() - time;
+        System.out.print("\nit take: "+ time1 + " to count");
       
     }
     
