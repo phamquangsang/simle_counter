9e0e4a5a5086a5cb4cc26753a34b24f07aea4109
Phạm Sang <phamngocquangsang@gmail.com>
2016-05-24 09:13:06 +0700

    initialize commit

---
+++ src/simplecounter/SimpleCounter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecounter;
+
+/**
+ *
+ * @author Quang Quang
+ */
+public class SimpleCounter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(long i=1 ; i<=100;++i){
+            System.out.print(""+i+" ");
+        }
+      
+    }
+    
+}
